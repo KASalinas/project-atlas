b90eca3c7b685afdab0230d248cc39afdcc140de
Karen Salinas <salinas.k62@gmail.com>
2026-07-29 15:22:51 -0400

    Create first custom web page

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<h1>Hello, Karen! 👋</h1>
+<p>Welcome to Project Atlas.</p>
+<p>I built my first JavaScript web application!</p>
+`
